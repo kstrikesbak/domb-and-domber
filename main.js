713703042da95b2bad58d964695efc38c310fc44
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-09 16:45:04 -0400

    not finished in the slightest

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
+function appendToUl(element) {
+    const append1 = document.querySelector(element);
+    // return append1.appendChild(element);
+}
+
+function appendToDiv(element) {
+    const div = document.querySelector('div');
+    div.id = 'lorem'
+    // return append2.appendChild(element);
+}
+
+function takesInTextAndReturnsAnLi(str) {
+    const newLi = document.createElement('li');
+    return newLi.innerText(str);
+}
+
+function takesInImageSourceToBeString(img,str) {
+    const imageSource = img.src(str);
+}
+
+function changeToSameClass(element1,element2) {
+    const newClass = element1.class;
+    element2.class = newClass;
+}
+
+function makesAnElementWithThatId(tag,id) {
+    const newElement = document.createElement(tag);
+    newElement.id = id;
+    return newElement.id;
+}
+
+function colorId(color,id) {
+    const newColor = document.querySelector(id)
+    newColor.style.color = color;
+}
+
+
+
+
+
+
 
