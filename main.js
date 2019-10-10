2eddf4c7c11b43b8d9e470e75a37290374f2f737
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-10 00:16:47 -0400

    all i can do for now

--- main.js
+++ main.js
@@ -4,42 +4,39 @@ function appendToUl(element) {
 }
 
 function appendToDiv(element) {
-    const append2 = document.querySelector('div');
-    append2.id = 'lorem'
+    const append2 = document.querySelector('div#lorem');
+    append2.appendChild(element);
     // return append2.appendChild(element);
 }
 
 function takesInTextAndReturnsAnLi(str) {
     const newLi = document.createElement('li');
-    return newLi.innerText(str);
+    const li = document.querySelector('li');
+    newLi.innerText(str);
+    return li.appendChild(newLi);
 }
 
 function takesInImageSourceToBeString(img,str) {
-    const imageSource = img.src(str);
+    const imageSource = document.querySelector(img);
 }
 
 function changeToSameClass(element1,element2) {
-    const newClass = element1.class;
-    element2.class = newClass;
+    const firstElement = document.querySelector(element1);
+    const secondElement = document.querySelector(element2);
+    return secondElement.class = firstElement.class
 }
 
 function makesAnElementWithThatId(tag,id) {
     const newElement = document.createElement(tag);
-    newElement.id = id;
-    return newElement.id;
+    newElement.id =id;
+    return newElement;
 }
 
-function colorId(color,id) {
-    const newColor = document.querySelector(id)
+function takesInTwoStringsAndDoesSomething(color, id) {
+    const newColor = document.querySelector(id);
     newColor.style.color = color;
 }
 
-
-const paragraph = document.createElement('h1');
-paragraph.innerText = 'HIIIIIII'
-appendToUl(paragraph);
-
-
-
-
-
+takesInTextAndReturnsAnLi('kejal-1');
+takesInTextAndReturnsAnLi('kejal-2');
+takesInTextAndReturnsAnLi('kejal-3');
